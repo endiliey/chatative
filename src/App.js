ecef8fd5f689f258dfe472593665fa2660f8dd70
endiliey <endiliey@gmail.com>
2017-10-05 02:50:18 +0800

    Share android & ios codebase

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> 
+          Hello world ! 
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
