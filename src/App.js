4c26f66cc914d407ce37b52c3d559b18bb69be34
endiliey <endiliey@gmail.com>
2018-03-16 17:10:58 +0800

    add chat screen UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Login from './components/Login';
+import Chat from './components/Chat';
 
 class App extends Component {
   render() {
     return (
-      <Login />
+      <Chat />
     );
   }
 }
---
+++ src/components/Chat.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat'
+
+class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    );
+  }
+}
+
+export default Chat;
