14392f536ee2287bfa5898df2239af10829fd141
endiliey <endiliey@gmail.com>
2018-03-16 23:16:32 +0800

    use redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import rootReducer from './reducers';
+import { fetchMessages } from './actions';
 import Login from './components/Login';
 import Chat from './components/Chat';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+const Screen = connect(
+  (state) => ({
+    _id: state.user._id
+  })
+)(( { _id }) => {
+  if ( _id !== null ) {
+    return (<Chat />);
+  } else {
+    return (<Login />);
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <Chat />
+      <Provider store={store}>
+        <Screen />
+      </Provider>
     );
   }
 }
